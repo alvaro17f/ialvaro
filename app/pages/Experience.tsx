2cce0b512848e37c7382e6ce6b394ca7f522bb54
Alvaro Garcia <89421445+alvaro17f@users.noreply.github.com>
2023-03-31 20:54:29 +0200

    .

--- app/pages/Experience.tsx
+++ app/pages/Experience.tsx
@@ -2,7 +2,7 @@
 import { useRef } from "react";
 import { domAnimation, LazyMotion, m, useScroll } from "framer-motion";
 import Link from "next/link";
-import data from "@/data/portfolio.json";
+import data from "@/data/experience.json";
 import Header from "@/components/Header";
 
 export default function Experience() {
@@ -15,34 +15,52 @@ export default function Experience() {
 	return (
 		<section id="experience">
 			<Header title="Experience" />
-			<div className="grid md:grid-cols-2 place-items-center">
-				{data.map(({ id, title, image, url, description }) => (
-					<LazyMotion key={id} features={domAnimation}>
-						<div ref={ref}>
-								<Link href={url} target="_blank">
-									<m.div
-										className="cursor-pointer w-[30dvw]"
-										initial={{ scale: 1, opacity: 0 }}
-										whileInView={{opacity: 1}}
-										viewport={{once: true}}
-										whileHover={{ scale: 1.1 }}
-										whileTap={{ scale: 1 }}
-										transition={{ duration: 0.7 }}
+			<div>
+				{data.map(
+					({ id, title, image, url, description, date_from, date_to }) => (
+						<LazyMotion key={id} features={domAnimation}>
+							<div ref={ref} className="grid md:grid-cols-[1fr_2fr] md:gap-10">
+								<m.div
+									className="grid gap-5 p-5 border border-dashed cursor-pointer h-60 mb-36 place-items-center rounded-xl"
+									initial={{ scale: 1, opacity: 0 }}
+									whileInView={{ opacity: 1 }}
+									viewport={{ once: true }}
+									whileHover={{ scale: 1.1 }}
+									whileTap={{ scale: 1 }}
+									transition={{ duration: 0.7 }}
+								>
+									<Link href={url} target="_blank">
+										<img src={image} alt={title} className="w-56 h-36" />
+									</Link>
+									<p>
+										{date_from} - {date_to}
+									</p>
+								</m.div>
+								<div className="mt-12">
+									<m.h1
+										key={title}
+										initial={{ opacity: 0 }}
+										whileInView={{ opacity: 1 }}
+										viewport={{once: false}}
+										transition={{ duration: 1 }}
+										className="mb-5 text-4xl md:text-6xl text-azama-primary w-[100%]"
 									>
-										<div className="h-32" />
-										<m.h1
-											key={title}
-											className="mb-5 text-4xl md:text-6xl text-azama-white w-[100%]"
-										>
-											{title}
-										</m.h1>
-										<img src={image} alt={title} />
-										<p>{description}</p>
-									</m.div>
-								</Link>
-						</div>
-					</LazyMotion>
-				))}
+										{title}
+									</m.h1>
+
+									<m.p
+										initial={{ opacity: 0 }}
+										whileInView={{ opacity: 1 }}
+										viewport={{once: false}}
+										transition={{ duration: 2 }}
+									>
+										{description}
+									</m.p>
+								</div>
+							</div>
+						</LazyMotion>
+					),
+				)}
 			</div>
 		</section>
 	);

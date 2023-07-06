c6112141e6b5030d2d75cad88c9eec0b78545aa5
Alvaro Garcia <89421445+alvaro17f@users.noreply.github.com>
2023-07-06 07:35:47 +0200

    .

--- components/LazyView.tsx
+++ components/LazyView.tsx
@@ -1,29 +1,29 @@
 import { domAnimation, LazyMotion, m } from "framer-motion";
 type Props = {
-	children: React.ReactNode;
-	once?: boolean;
-	duration?: number;
+  children: React.ReactNode;
+  once?: boolean;
+  duration?: number;
 };
 
 export default function LazyView({
-	children,
-	once = true,
-	duration = 1,
+  children,
+  once = true,
+  duration = 1,
 }: Props) {
-	return (
-		<LazyMotion features={domAnimation}>
-			<m.div
-				initial="hidden"
-				whileInView="visible"
-				viewport={{ once: once }}
-				transition={{ duration }}
-				variants={{
-					visible: { opacity: 1 },
-					hidden: { opacity: 0 },
-				}}
-			>
-				{children}
-			</m.div>
-		</LazyMotion>
-	);
+  return (
+    <LazyMotion features={domAnimation}>
+      <m.div
+        initial="hidden"
+        whileInView="visible"
+        viewport={{ once: once }}
+        transition={{ duration }}
+        variants={{
+          visible: { opacity: 1 },
+          hidden: { opacity: 0 },
+        }}
+      >
+        {children}
+      </m.div>
+    </LazyMotion>
+  );
 }
--- components/Wobble.tsx
+++ components/Wobble.tsx
@@ -1,69 +1,69 @@
 "use client";
 import React, { useId, useState } from "react";
 import {
-	m,
-	LazyMotion,
-	domAnimation,
-	useAnimationControls,
+  m,
+  LazyMotion,
+  domAnimation,
+  useAnimationControls,
 } from "framer-motion";
 
 const Text = ({
-	children,
-	style,
+  children,
+  style,
 }: { children: React.ReactNode; style?: string }) => {
-	const controls = useAnimationControls();
-	const [isPlaying, setIsPlaying] = useState(false);
+  const controls = useAnimationControls();
+  const [isPlaying, setIsPlaying] = useState(false);
 
-	const rubberBand = () => {
-		controls.start({
-			transform: [
-				"scale3d(1, 1, 1)",
-				"scale3d(1.4, .55, 1)",
-				"scale3d(.75, 1.25, 1)",
-				"scale3d(1.25, .85, 1)",
-				"scale3d(.9, 1.05, 1)",
-				"scale3d(1, 1, 1)",
-			],
-			transition: {
-				times: [0, 0.4, 0.6, 0.7, 0.8, 0.9],
-			},
-		});
-		setIsPlaying(true);
-	};
+  const rubberBand = () => {
+    controls.start({
+      transform: [
+        "scale3d(1, 1, 1)",
+        "scale3d(1.4, .55, 1)",
+        "scale3d(.75, 1.25, 1)",
+        "scale3d(1.25, .85, 1)",
+        "scale3d(.9, 1.05, 1)",
+        "scale3d(1, 1, 1)",
+      ],
+      transition: {
+        times: [0, 0.4, 0.6, 0.7, 0.8, 0.9],
+      },
+    });
+    setIsPlaying(true);
+  };
 
-	return (
-		<LazyMotion features={domAnimation}>
-			<m.span
-				className={`inline-block cursor-default text-6xl md:text-9xl ${style}`}
-				animate={controls}
-				onMouseOver={() => !isPlaying && rubberBand()}
-				onAnimationComplete={() => setIsPlaying(false)}
-				aria-label="wobble"
-			>
-				{children}
-			</m.span>
-		</LazyMotion>
-	);
+  return (
+    <LazyMotion features={domAnimation}>
+      <m.span
+        className={`inline-block cursor-default text-6xl md:text-9xl ${style}`}
+        animate={controls}
+        onMouseOver={() => !isPlaying && rubberBand()}
+        onAnimationComplete={() => setIsPlaying(false)}
+        aria-label="wobble"
+      >
+        {children}
+      </m.span>
+    </LazyMotion>
+  );
 };
 
 type Props = {
-	sentence: string;
-	style?: string;
+  sentence: string;
+  style?: string;
 };
 
 export default function Wobble({ sentence, style }: Props) {
-	const uuid = useId();
+  const uuid = useId();
 
-	return (
-		<>
-			{sentence.split("").map((letter, index) => {
-				return (
-					<Text style={style} key={`${uuid}-${index}`}>
-						{letter === " " ? "\u00A0" : letter}
-					</Text>
-				);
-			})}
-			<br />
-		</>
-	);
+  return (
+    <>
+      {sentence.split("").map((letter, index) => {
+        return (
+          <Text style={style} key={`${uuid}-${index}`}>
+            {letter === " " ? "\u00A0" : letter}
+          </Text>
+        );
+      })}
+      <br />
+    </>
+  );
 }

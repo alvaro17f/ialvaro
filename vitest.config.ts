dabd28550ffde6ddd9a2c766e5346b952ac53eac
Alvaro <89421445+alvaro17f@users.noreply.github.com>
2024-12-16 09:28:26 +0100

    chore: update

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,17 +1,17 @@
 /// <reference types="vitest" />
 import react from "@vitejs/plugin-react";
 import path from "node:path";
-import { getViteConfig } from "astro/config";
+import { defineConfig } from "vite";
 
-export default getViteConfig({
-	plugins: [react()],
-	test: {
-		globals: true,
-		environment: "happy-dom",
-		environmentMatchGlobs: [["src/**", "happy-dom"]],
-		include: ["**/*.{test,spec,test.integration}.?(c|m)[t|j]s?(x)"],
-		alias: {
-			src: path.resolve(__dirname, "./src"),
-		},
-	},
+export default defineConfig({
+  plugins: [react()],
+  test: {
+    globals: true,
+    environment: "happy-dom",
+    environmentMatchGlobs: [["src/**", "happy-dom"]],
+    include: ["**/*.{test,spec,test.integration}.?(c|m)[t|j]s?(x)"],
+    alias: {
+      src: path.resolve(__dirname, "./src"),
+    },
+  },
 });

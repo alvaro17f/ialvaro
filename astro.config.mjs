7e0f91fa2c7dfa7c354225b8595cd09a68edb4bd
Alvaro <89421445+alvaro17f@users.noreply.github.com>
2025-01-30 18:45:22 +0100

    fix: add astrojs node integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,23 @@
 import { defineConfig } from 'astro/config'
 
+import node from '@astrojs/node'
 import react from '@astrojs/react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://astro.build/config
 export default defineConfig({
   integrations: [react()],
 
   vite: {
     plugins: [tailwindcss()],
   },
+
+  server: {
+    port: 4321,
+    host: true,
+  },
+
+  output: 'server',
+  adapter: node({
+    mode: 'standalone',
+  }),
 })

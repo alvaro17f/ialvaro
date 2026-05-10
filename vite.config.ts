73e607f80c1738074cb9c1b301140ad2c00fae7c
Alvaro <89421445+alvaro17f@users.noreply.github.com>
2026-05-10 23:20:24 +0200

    fix: allow host zenon.lan in vite dev server

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+	server: {
+		allowedHosts: ["zenon.lan"],
+	},
 	resolve: {
 		alias: {
 			"@": path.resolve("./src"),
